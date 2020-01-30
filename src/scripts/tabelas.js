f64cc34e497d3d7caa3784d6ba8ac6c6b1d28f4b
Ubuntu <joaomarcoslopes@id.uff.br>
2020-01-30 17:41:02 -0300

    form alunos

--- src/scripts/tabelas.js
+++ src/scripts/tabelas.js
@@ -1,28 +1,39 @@
-$('document').ready(() => tabelas())
+function validacao(form_id, form_config) {
+	$(form_id).validate(form_config)
+}
 
-function tabelas() {
-	validacao('#formulario', {
+function validar() {
+	validacao('#formulario-alunos', {
 		rules: {
-			nome: { required: true, minlength: 5 },
-			senha: { required: true, minlength: 5 },
-			email: { required: true }
+			nome: { required: true, minlength: 3, maxlength: 50 },
+			senha: { required: true, minlength: 3 },
+			confirmaSenha: { required: true, minlength: 3, equalTo: '#senha' },
+			email: { required: true, minlength: 3, maxlength: 50 },
+			cpf: { required: true }
 		},
 		messages: {
 			nome: {
 				required: 'Campo nome é obrigatório',
-				minlength: 'O nome deve possuir no mínimo 5 caracteres'
+				minlength: 'O nome deve possuir no mínimo 3 caracteres'
 			},
 			senha: {
 				required: 'Campo senha é obrigatório',
-				minlength: 'A senha deve possuir no mínimo 5 caracteres'
+				minlength: 'A senha deve possuir no mínimo 3 caracteres'
+			},
+			confirmaSenha: {
+				required: 'Campo confirmar senha é obrigatório',
+				minlength: 'A senha deve possuir no mínimo 3 caracteres',
+				equalTo: 'As senhas devem coincidir'
 			},
 			email: {
-				required: 'Campo email é obrigatório'
+				required: 'Campo email é obrigatório',
+				email: 'Por favor, insira um email válido'
+			},
+			cpf: {
+				required: 'Campo CPF é obrigatório'
 			}
 		}
 	})
 }
 
-function validacao(form_id, form_config) {
-	$(form_id).validate(form_config)
-}
+$('document').ready(() => validar())
